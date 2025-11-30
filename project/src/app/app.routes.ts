0943ed375cd7ef7b0c974220e812202cc11f1368
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-30 23:26:18 +0100

    finsh router configuration

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -2,15 +2,38 @@ import { Routes } from '@angular/router';
 import { provideRouter } from '@angular/router';
 
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+
 import {FeaturedProductsComponent} from "./pages/featured-products/featured-products.component";
 import {ProductDetailsComponent} from "./pages/product-details/product-details.component";  
-import { HomeButtonComponent } from './components/home-button/home-button.component';
+import { ProductCategoryComponent } from './pages/product-category/product-category.component';
 
 export const routes: Routes = [
 
-    {
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
         path: '',
-        component: MainLayoutComponent,
+        component: FeaturedProductsComponent, // página inicial
+      },
+      {
+        path: 'movies',
+        component: ProductCategoryComponent
+      },
+      {
+        path: 'books',
+        component: ProductCategoryComponent
+      },
+      {
+        path: 'video-games',
+        component: ProductCategoryComponent
+      },
+      {
+        path: 'product/:productName',
+        component: ProductDetailsComponent
+      }
+    ]
+  }
 
-    }
 ];
