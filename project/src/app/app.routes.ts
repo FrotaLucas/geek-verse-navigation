7bcb1a2c1591111f4f6eb653e01bb774edeb8fa6
FrotaLucas <lucasdiasfrota@gmail.com>
2025-11-30 22:39:56 +0100

    import all components

--- project/src/app/app.routes.ts
+++ project/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
-export const routes: Routes = [];
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import {FeaturedProductsComponent} from "./pages/featured-products/featured-products.component";
+import {ProductDetailsComponent} from "./pages/product-details/product-details.component";  
+import { HomeButtonComponent } from './components/home-button/home-button.component';
+
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: MainLayoutComponent,
+
+    }
+];
